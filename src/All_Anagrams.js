785ca77ea76a29169f9d6a01472575fa22569cf2
JW Garrison <jwolfgarrison@gmail.com>
2017-01-14 18:51:55 -0500

    Pass spec #1

--- src/All_Anagrams.js
+++ src/All_Anagrams.js
@@ -1,13 +1,10 @@
-const allAnagrams = (string) => {
-  return [...string].reduce((anagram, character, index, array) => {
-    return [...anagram, ...character];
-  }, []);
-};
+const allAnagrams = str => [str];
 
-console.log(allAnagrams('abc'));
-
-
-// var result = inputArray.reduce(function permute(res, item, key, arr) {
-//   console.log(res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item););
-//   return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
-// }, []);
+console.log('------------------------------');
+console.log('1) allAnagrams(\'a\')');
+console.log(' ');
+console.log('INPUT: a');
+console.log('EXPECTED: [\'a\']');
+console.log('OUTPUT: ', allAnagrams('a'));
+console.log('------------------------------');
+console.log(' ');
