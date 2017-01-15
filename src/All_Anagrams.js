16eee60cc1e6a62891b33091cd69a3ef3cd74593
JW Garrison <jwolfgarrison@gmail.com>
2017-01-14 23:29:35 -0500

    Solve All Anagrams

--- src/All_Anagrams.js
+++ src/All_Anagrams.js
@@ -1,10 +1,64 @@
-const allAnagrams = str => [str];
+const allAnagrams = (str) => {
+  const anagrams = {};
 
-console.log('------------------------------');
-console.log('1) allAnagrams(\'a\')');
-console.log(' ');
-console.log('INPUT: a');
-console.log('EXPECTED: [\'a\']');
-console.log('OUTPUT: ', allAnagrams('a'));
-console.log('------------------------------');
-console.log(' ');
+  (function find(acc = '', src = str) {
+    if (!src.length) anagrams[acc] = true;
+
+    [...src].forEach((curr, i) => {
+      const newAcc = acc + curr;
+      const newSrc = src.slice(0, i) + src.slice(i + 1);
+      find(newAcc, newSrc);
+    });
+  }());
+
+  return Object.keys(anagrams);
+};
+
+
+/* Recursive routine broken down
+
+letters.forEach((char, index) => {
+  const anagram = char;
+  const remaining = str.slice(0, index) + str.slice(index + 1);
+  const remainingLetter = remaining.split('');
+  remainingLetter.forEach((char2, index2, array) => {
+    const anagram2 = anagram + char2;
+    const remaining2 = array.join('').slice(0, index2) + array.slice(index2 + 1);
+    const remainingLetter2 = remaining2.split('');
+    remainingLetter2.forEach((char3, index3, array3) => {
+      const anagram3 = anagram2 + char3;
+      anagrams.push(anagram3);
+    });
+  });
+});
+
+*/
+
+const inputs = [
+  'a',
+  'abc',
+  'ab',
+  'apps',
+];
+
+const expected = [
+  ['a'],
+  ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'],
+  ['ab', 'ba'],
+  ['apps', 'apsp', 'aspp', 'paps', 'pasp', 'ppas', 'ppsa', 'psap', 'pspa', 'sapp', 'spap', 'sppa'],
+];
+
+const logger = (is, o) => {
+  is.forEach((i, idx) => {
+    console.log('------------------------------');
+    console.log(`${idx + 1}) allAnagrams(${i})`);
+    console.log(' ');
+    console.log(`INPUT: ${i}`);
+    console.log('EXPECTED: ', o[idx]);
+    console.log('OUTPUT: ', allAnagrams(i));
+    console.log('------------------------------');
+    console.log(' ');
+  });
+};
+
+logger(inputs, expected);
